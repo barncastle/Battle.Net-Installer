5872c18df7366e4a1301f4f71d73ff3814939524
Barncastle <spearman.tom@gmail.com>
2025-06-09 16:41:20 +0100

    update agent process code

--- BNetInstaller/AgentApp.cs
+++ BNetInstaller/AgentApp.cs
@@ -10,30 +10,25 @@ namespace BNetInstaller;
 
 internal sealed class AgentApp : IDisposable
 {
-    public const int Port = 5050;
-
     public readonly AgentEndpoint AgentEndpoint;
     public readonly InstallEndpoint InstallEndpoint;
     public readonly UpdateEndpoint UpdateEndpoint;
     public readonly RepairEndpoint RepairEndpoint;
     public readonly VersionEndpoint VersionEndpoint;
 
-    private readonly string _agentPath;
     private readonly Process _process;
+    private readonly int _port;
     private readonly AgentClient _client;
 
     public AgentApp()
     {
-        _agentPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Battle.net", "Agent", "Agent.exe");
-
-        if (!StartProcess(out var process))
+        if (!StartProcess(out _process, out _port))
         {
             Console.WriteLine("Please ensure Battle.net is installed and has recently been signed in to.");
             Environment.Exit(0);
         }
 
-        _process = process;
-        _client = new(Port);
+        _client = new(_port);
 
         AgentEndpoint = new(_client);
         InstallEndpoint = new(_client);
@@ -42,26 +37,60 @@ internal sealed class AgentApp : IDisposable
         VersionEndpoint = new(_client);
     }
 
-    private bool StartProcess(out Process process)
+    private static bool StartProcess(out Process process, out int port)
     {
-        if (!File.Exists(_agentPath))
+        (process, port) = (null, -1);
+
+        var agentPath = GetAgentPath();
+
+        if (!File.Exists(agentPath))
         {
-            process= null;
             Console.WriteLine("Unable to find Agent.exe.");
             return false;
         }
 
         try
         {
-            process = Process.Start(_agentPath, $"--port={Port}");
+            process = Process.Start(new ProcessStartInfo(agentPath)
+            {
+                Arguments = "--internalclienttools",
+                UseShellExecute = true,
+            });
+
+            // detect listening port
+            while (!process.HasExited && port == -1)
+            {
+                Thread.Sleep(250);
+                port = NativeMethods.GetProcessListeningPort(process.Id);
+            }
+
+            if (process.HasExited || port == -1)
+            {
+                Console.WriteLine("Unable to connect to Agent.exe.");
+                return false;
+            }
+
             return true;
         }
         catch (Win32Exception)
         {
-            process = null;
             Console.WriteLine("Unable to start Agent.exe.");
             return false;
         }
+    }
+
+    private static string GetAgentPath()
+    {
+        var agentDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Battle.net", "Agent");
+        var parentPath = Path.Combine(agentDirectory, "Agent.exe");
+        var parentVersion = 0;
+
+        // read parent Agent.exe version
+        if (File.Exists(parentPath))
+            parentVersion = FileVersionInfo.GetVersionInfo(parentPath).ProductPrivatePart;
+
+        // return expected child Agent path
+        return Path.Combine(agentDirectory, $"Agent.{parentVersion}", "Agent.exe");
     }
 
     public void Dispose()
---
+++ BNetInstaller/NativeMethods.cs
@@ -0,0 +1,63 @@
+﻿using System.Buffers.Binary;
+using System.Runtime.InteropServices;
+
+namespace BNetInstaller;
+
+internal static partial class NativeMethods
+{
+    private const int MIB_TCPROW2_SIZE = 0x1C;
+    private const int MIB_TCP_STATE_LISTEN = 2;
+
+    [LibraryImport("iphlpapi.dll", EntryPoint = "GetTcpTable2")]
+    private static partial int GetTcpTable(nint tcpTable, ref int size, [MarshalAs(UnmanagedType.Bool)] bool bOrder);
+
+    public static int GetProcessListeningPort(int pid)
+    {
+        var size = 0;
+
+        // get MIB_TCPTABLE2 size
+        _ = GetTcpTable(0, ref size, false);
+
+        var buffer = Marshal.AllocHGlobal(size);
+        var pBuffer = buffer;
+
+        // read MIB_TCPTABLE2
+        if (GetTcpTable(pBuffer, ref size, false) != 0)
+            return -1;
+
+        try
+        {
+            var dwNumEntries = Marshal.ReadInt32(pBuffer); // MIB_TCPTABLE2->dwNumEntries
+            pBuffer += sizeof(int);
+
+            for (var i = 0; i < dwNumEntries; i++)
+            {
+                var row = Marshal.PtrToStructure<MIB_TCPROW2>(pBuffer);
+                pBuffer += MIB_TCPROW2_SIZE;
+
+                if (row.dwOwningPid == pid && row.dwState == MIB_TCP_STATE_LISTEN)
+                {
+                    return BinaryPrimitives.ReverseEndianness((ushort)row.dwLocalPort);
+                }
+            }
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(buffer);
+        }
+
+        return -1;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct MIB_TCPROW2
+    {
+        public int dwState;
+        public int dwLocalAddr;
+        public int dwLocalPort;
+        public int dwRemoteAddr;
+        public int dwRemotePort;
+        public int dwOwningPid;
+        public int dwOffloadState;
+    }
+}
